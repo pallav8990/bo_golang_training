9d92b06324410e4f642235c665e1d651c86274a4
pallav8990 <pallavunix@gmail.com>
2024-07-10 11:25:19 +0530

    day3

---
+++ Day1/Results/fibinacci.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+func main() {
+
+	f := fibonacci()
+
+	// Print the first 10 Fibonacci numbers.
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
